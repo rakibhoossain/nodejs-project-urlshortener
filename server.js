9b4535f90064786767d96e048ae47ee1db0dd9d0
Rakib Hossain <rakibhoossain@gmail.com>
2019-10-07 01:01:26 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const urlControler = require('./urlController');
 
 var cors = require('cors');
 
@@ -12,12 +14,20 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose
+  .connect(process.env.MONGOLAB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -26,10 +36,11 @@ app.get('/', function(req, res){
 });
 
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// ... what to do when the user submits/POSTs (our submit button is set to POST) a new long-form URL...
+app.post("/api/shorturl/new", urlControler.postLongUrl);
+
+// ... and what to do when a short-url is targeted...
+app.get("/api/shorturl/:short_url", urlControler.getShortUrl);
 
 
 app.listen(port, function () {
